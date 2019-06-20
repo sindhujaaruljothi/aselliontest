2601475b3414d5e1eb19fc263a1e92cd27afd93f
sindhujaaruljothi <sindhu.shil@gmail.com>
2019-06-20 12:17:47 +0200

    First project skeleton push for asellion test

---
+++ src/main/java/com/sindhu/aselliontest/aselliontest/AselliontestApplication.java
@@ -0,0 +1,13 @@
+package com.sindhu.aselliontest.aselliontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AselliontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AselliontestApplication.class, args);
+	}
+
+}
